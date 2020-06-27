13e53ead886099969c37cd1c721ecf12469d7b1d
Illia Goldin <igoldin@iboats.com>
2020-06-26 23:20:07 -0600

    Draft

---
+++ hover-test/hover-test.js
@@ -0,0 +1,19 @@
+
+// Create small div
+let redDiv = document.createElement("div");
+redDiv.style.width = "50px";
+redDiv.style.height = "50px";
+redDiv.style.background = "orange";
+redDiv.style.position = "absolute"
+
+// Create an HTML collection of immediate children of <body> tag
+let y = document.body.children;
+
+// Add small div to element
+for (let i = 0; i < y.length; i++) {
+    y[i].onmouseover = function () {
+        this.appendChild(redDiv);
+    };
+}
+
+ssh - keygen - t rsa - b 4096 - C "squl2squl@hotmail.com"
